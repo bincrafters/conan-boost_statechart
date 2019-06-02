cded1d38d52ac2a765b164b750f044ddd69d3235
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,25 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostStatechartConan(base.BoostBaseConan):
     name = "boost_statechart"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_statechart"
-    lib_short_names = ["statechart"]
-    header_only_libs = ["statechart"]
-    b2_requires = [
-        "boost_assert",
-        "boost_bind",
-        "boost_config",
-        "boost_conversion",
-        "boost_core",
-        "boost_detail",
-        "boost_function",
-        "boost_mpl",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_thread",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
